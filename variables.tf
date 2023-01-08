798a0d63ab7f56ba80ec4959eef680299a5b5442
VITALYALGOSEC <96118544+VITALYALGOSEC@users.noreply.github.com>
2023-01-08 12:27:14 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "org_id" {
+  description = "Organization's id"
+  type        = string
+}
+
+variable "billing_account" {
+  description = "Billing account ID would be assosiated with the new created project"
+  type        = string
+}
